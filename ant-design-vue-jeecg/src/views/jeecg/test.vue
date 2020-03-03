83eb93b4a82834226041f554b7ab1ff79cf6b640
Tony Wu <tonywu@TonydeMacBook-Pro.local>
2020-03-03 14:17:14 +0800

    auto_code_finish

---
+++ ant-design-vue-jeecg/src/views/jeecg/test.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>{{message}}</div>
+</template>
+
+<script>
+    import { getAction } from '@/api/manage';
+    export default {
+      name: "message",
+      data() {
+        return {
+          message: ""
+        }
+      },
+      methods: {
+        name() {
+
+        }
+      },
+      created() {
+        getAction("/test/jeecgTest/hello").then(res=>{
+          console.log("...")
+          this.message=res.result;
+        })
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/test/JeecgTestController.java
@@ -0,0 +1,4 @@
+package org.jeecg.modules.test;
+
+public class JeecgTestController {
+}
